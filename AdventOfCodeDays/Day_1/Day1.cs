9951bcaafd19082250ee14be8de78b5c8ba354bd
Aaron Bush <aaronodellbush@gmail.com>
2022-12-06 22:50:02 -0500

    commented day 1 & 2

--- AdventOfCodeDays/Day_1/Day1.cs
+++ AdventOfCodeDays/Day_1/Day1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Advent_Of_Code_2022
 {
@@ -9,48 +10,48 @@ namespace Advent_Of_Code_2022
         private static string dataLocation = "Day_1/AdventData.txt";
         public static void Star1()
         {
-            StreamReader reader = new StreamReader(dataLocation);
+            StreamReader reader = new StreamReader(dataLocation); //Get all data
             string line;
 
             int max = 0;
             int current = 0;
-            while ((line = reader.ReadLine()) != null)
+            while ((line = reader.ReadLine()) != null) //Read it in, line by line
             {
-                if (line != "") { current += int.Parse(line); }
-                else
+                if (line != "") { current += int.Parse(line); } //If line isn't empty, add its value to the current sum
+                else //If line is empty
                 {
-                    if (current > max)
+                    if (current > max) //Compare current to max, save current if it is the new max
                     {
                         max = current;
                     }
-                    current = 0;
+                    current = 0; //Reset current
                 }
             }
 
-            Console.WriteLine($"Day 1 Star 1 Answer: {max}");
+            Console.WriteLine($"Day 1 Star 1 Answer: {max}"); //Output largest total
             reader.Close();
         }
         public static void Star2()
         {
-            StreamReader reader = new StreamReader(dataLocation);
+            StreamReader reader = new StreamReader(dataLocation); //Get all data
             string line;
 
             List<int> sums = new List<int>();
             int current = 0;
 
-            while ((line = reader.ReadLine()) != null)
+            while ((line = reader.ReadLine()) != null) //Read it in, line by line
             {
-                if (line != "") { current += int.Parse(line); }
-                else
+                if (line != "") { current += int.Parse(line); } //If line isn't empty, add its value to the current sum
+                else //If line is empty, add the current to the list of sums
                 {
                     sums.Add(current);
-                    current = 0;
+                    current = 0; //Reset current
                 }
             }
-            sums.Sort();
-            sums.Reverse();
+            sums.Sort(); //Sort list by smallest -> largest
+            sums.Reverse(); //Reverse list
 
-            Console.WriteLine($"Day 1 Star 2 Answer: {sums[0] + sums[1] + sums[2]}\n");
+            Console.WriteLine($"Day 1 Star 2 Answer: {sums[0] + sums[1] + sums[2]}\n"); //Output sum of three largest totals
             reader.Close();
         }
     }
--- AdventOfCodeDays/Day_2/Day2.cs
+++ AdventOfCodeDays/Day_2/Day2.cs
@@ -14,18 +14,20 @@ namespace Advent_Of_Code_2022
             int total = 0;
             while ((line = reader.ReadLine()) != null)
             {
-                string[] split = line.Split(" ");
-                char enemyPlay = split[0][0];
-                char yourPlay = split[1][0];
+                string[] split = line.Split(" "); //Split line by space
+                char enemyPlay = split[0][0]; //Get enemy input
+                char yourPlay = split[1][0]; //Get "my" input
 
                 //A && X == Rock, B && Y == Paper, C && Z == Scissors
+                //If -> Tie, add three to total, Else If -> I Win, add six to total
                 if (enemyPlay == 'A' && yourPlay == 'X' || enemyPlay == 'B' && yourPlay == 'Y' || enemyPlay == 'C' && yourPlay == 'Z') { total += 3; }
                 else if (enemyPlay == 'A' && yourPlay == 'Y' || enemyPlay == 'B' && yourPlay == 'Z' || enemyPlay == 'C' && yourPlay == 'X') { total += 6; }
 
+                //Convert play to num value, add it to total. X = 1, Y = 2, Z = 3
                 total += yourPlay - 87;
             }
 
-            Console.WriteLine($"Day 2 Star 1 Answer: {total}");
+            Console.WriteLine($"Day 2 Star 1 Answer: {total}"); //Return total of points earned in all games
             reader.Close();
         }
         public static void Star2()
@@ -36,18 +38,19 @@ namespace Advent_Of_Code_2022
             int total = 0;
             while ((line = reader.ReadLine()) != null)
             {
-                string[] split = line.Split(" ");
-                char enemyPlay = split[0][0];
-                char gameResult = split[1][0];
+                string[] split = line.Split(" "); //Split line by space
+                char enemyPlay = split[0][0]; //Get enemy input
+                char gameResult = split[1][0]; //Get outcome of the game
 
                 //A == Rock, B == Paper, C == Scissors
                 //X == Lose, Y == Draw, Z == Win
+                //If -> Lose, add Input Score, Else If -> Tie, add Input Score plus 3, Else If -> Win, add Input Score plus 6
                 if (gameResult == 'X') { total += (enemyPlay - 63) % 3 + 1; } //A => 3, B => 1, C => 2
-                if (gameResult == 'Y') { total += 3 + enemyPlay - 64; }
-                if (gameResult == 'Z') { total += 6 + (enemyPlay - 61) % 3 + 1; } //A => 2, B => 3, C => 1, plus 6 for Winning
+                else if (gameResult == 'Y') { total += 3 + enemyPlay - 64; }
+                else if (gameResult == 'Z') { total += 6 + (enemyPlay - 61) % 3 + 1; } //A => 2, B => 3, C => 1, plus 6 for Winning
             }
 
-            Console.WriteLine($"Day 2 Star 2 Answer: {total}\n");
+            Console.WriteLine($"Day 2 Star 2 Answer: {total}\n"); //Return total of points earned in all games
             reader.Close();
         }
     }
